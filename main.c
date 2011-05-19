7d9ed8621f1077136b23f6e2f0dbb25fde083b98
Michael Forney <mforney@mforney.org>
2011-05-18 22:48:13 -0700

    Add main file and buildsystem

---
+++ main.c
@@ -0,0 +1,44 @@
+/* One Astronaut: main.c
+ *
+ * Copyright (c) 2011 Michael Forney <mforney@mforney.org>
+ *
+ * This file is a part of One Astronaut.
+ *
+ * One Astronaut is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2, as published by
+ * the Free Software Foundation.
+ *
+ * One Astronaut is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * One Astronaut.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <allegro5/allegro.h>
+
+bool running = true;
+
+void setup()
+{
+    al_init();
+    al_create_display(800, 600);
+}
+
+void run()
+{
+    while (running)
+    {
+    }
+}
+
+int main(int argc, char * argv[])
+{
+    setup();
+    run();
+}
+
+// vim: fdm=syntax fo=croql et sw=4 sts=4 ts=8
+
